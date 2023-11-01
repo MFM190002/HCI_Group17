7d60469f3662adbd2e2807f4d8e1543fa3763d7b
justin-doan <jmdoan8@gmail.com>
2023-11-01 12:24:21 -0500

    changes to journey and home page

--- my-react-app/src/pages/HomePage/HomePage.jsx
+++ my-react-app/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./HomePage.css";
+import {Link} from 'react-router-dom';
 
 function HomePage() {
   return (
@@ -12,34 +13,19 @@ function HomePage() {
             alt="Rectangle"
             src="https://cdn.animaapp.com/projects/65408ceb2ddc83e6bd71570e/releases/65408e081cff822e1265c0f3/img/rectangle-19-2.svg"
           />
-          <div className="text-wrapper-2">Sign Out</div>
+          
+          <Link to="/" className="text-wrapper-2">
+            Sign Out
+          </Link>
         </div>
-        <div className="text-wrapper-3">Upcoming Checkpoints</div>
+        <div className="text-wrapper-3">Your Upcoming Checkpoints</div>
         <div className="text-wrapper-4">Friends Leaderboard</div>
         <div className="overlap-group">
           <div className="text-wrapper-5">Progress</div>
-          <div className="text-wrapper-6">Goal</div>
           <div className="rectangle-2" />
         </div>
-        <div className="overlap-2">
-          <div className="group">
-            <div className="overlap-group-2">
-              <div className="text-wrapper-7">Post Malone</div>
-              <div className="rectangle-3" />
-            </div>
-          </div>
-          <div className="text-wrapper-7">{""}</div>
-        </div>
-        <div className="friend-wrapper">
-          <div className="friend">
-            <div className="overlap-group-2">
-              <div className="text-wrapper-7">Harry Styles</div>
-              <div className="rectangle-3" />
-            </div>
-          </div>
-        </div>
         <div className="overlap-wrapper">
-          <div className="overlap-3">
+          <div className="view-friends">
             <img
               className="img"
               alt="Rectangle"
@@ -58,17 +44,19 @@ function HomePage() {
             <div className="text-wrapper-8">View Checkpoints</div>
           </div>
         </div>
-        <div className="group-2">
-          <div className="rectangle-4" />
-          <div className="text-wrapper-9">Draft College Essay</div>
-        </div>
-        <div className="group-3">
-          <div className="rectangle-4" />
-          <div className="text-wrapper-10">Start Application to UTD</div>
-        </div>
-        <div className="group-4">
-          <div className="rectangle-4" />
-          <div className="text-wrapper-11">Fill out FAFSA</div>
+        <div className="checkpoints" >
+          <div className="group-2">
+            <div className="rectangle-4" />
+            <div className="text-wrapper-9">Research 5 Colleges</div>
+          </div>
+          <div className="group-3">
+            <div className="rectangle-4" />
+            <div className="text-wrapper-10">Fill out FAFSA</div>
+          </div>
+          <div className="group-4">
+            <div className="rectangle-4" />
+            <div className="text-wrapper-11">Create your resume</div>
+          </div>
         </div>
       </div>
     </div>
--- my-react-app/src/pages/JourneyPage/JourneyPage.jsx
+++ my-react-app/src/pages/JourneyPage/JourneyPage.jsx
@@ -1,27 +1,71 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./JourneyPage.css";
+import { Link } from "react-router-dom";
+import submit from "./icons8-enter-64.png";
 
 export const JourneyPage = () => {
+  const navigate = useNavigate();
+  const [isCustomGoal, setIsCustomGoal] = useState(false);
+  const [selectedGoal, setSelectedGoal] = useState(""); // Initialize selectedGoal state
+
+  const handleButtonClick = () => {
+    navigate("/home");
+  };
+
+  const handleInputKeyPress = (event) => {
+    if (event.key === "Enter") {
+      navigate("/home");
+    }
+  };
+
+  const handleGoalSelect = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedGoal(selectedValue);
+
+    if (selectedValue === "Custom Goal") {
+      setIsCustomGoal(true);
+    } else {
+      setIsCustomGoal(false);
+    }
+  };
+
   return (
     <div className="journey">
       <div className="div">
         <div className="text-wrapper">IDCollege</div>
         <div className="overlap">
           <div className="group">
-            <div className="overlap-group">
-            <input type="text" className="text-wrapper-5" placeholder="Apply to UTD" />
-            </div>
+            {isCustomGoal ? ( // Show custom input when isCustomGoal is true
+              <input
+                type="text"
+                className="text-wrapper-5 input-box"
+                placeholder="Type your goal"
+                onKeyPress={handleInputKeyPress}
+              />
+            ) : (
+              // Show drop-down list when isCustomGoal is false
+              <select
+                name="goals"
+                className="text-wrapper-5 input-box"
+                value={selectedGoal}
+                onChange={handleGoalSelect}
+              >
+                <option value="Curated College Application Guide">
+                  Follow our Curated College Application Guide
+                </option>
+                <option value="Curated Scholarship Application Guide">
+                  Follow our Curated Scholarship Application Guide
+                </option>
+                <option value="Custom Goal">I want to set my own goal</option>
+              </select>
+            )}
+            <Link to="/home">
+              <img className="img" alt="Rectangle" src={submit} />
+            </Link>
           </div>
-          <Link to="/home">
-            <img
-              className="img"
-              alt="Rectangle"
-              src="https://cdn.animaapp.com/projects/65408ceb2ddc83e6bd71570e/releases/65408e081cff822e1265c0f3/img/rectangle-18.svg"
-            />
-          </Link>
         </div>
-        <div className="text-wrapper-3">Journey Start Goal</div>
+        <div className="text-wrapper-3">Set your college application goal</div>
       </div>
     </div>
   );
