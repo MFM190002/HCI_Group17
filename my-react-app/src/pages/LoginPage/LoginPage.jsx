de638f127cd8b8eb878d46493b34246e205b3275
jin <hyeongjinjoo82@gmail.com>
2023-11-01 14:24:07 -0500

    Updated LoginPage code

--- my-react-app/src/pages/LoginPage/LoginPage.jsx
+++ my-react-app/src/pages/LoginPage/LoginPage.jsx
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.css";
 import { Link } from "react-router-dom";
+
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted credentials:", username, password);
+
+    // TODO: Handle authentication logic here
+  };
+
   return (
     <div className="login">
       <div className="div">
         <div className="text-wrapper">IDCollege</div>
-        <div className="overlap-group">
-          <div className="rectangle" />
-          <div className="rectangle" />
-          <div className="text-wrapper-2">username</div>
-          <div className="rectangle" />
-          <div className="rectangle" />
-          <div className="rectangle" />
-          <div className="text-wrapper-2">username</div>
-          <div className="rectangle" />
-          <div className="rectangle" />
-          <div className="rectangle" />
-          <div className="text-wrapper-2">username</div>
-        </div>
-        <div className="overlap">
-          <div className="text-wrapper-2">password</div>
-          <div className="rectangle" />
-          <div className="text-wrapper-2">password</div>
-          <div className="rectangle" />
-          <div className="text-wrapper-2">password</div>
-        </div>
-        <div className="overlap-2">
-          <div className="rectangle-2" />
-          <div className="text-wrapper-3">Login</div>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="overlap-group">
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="username"
+              className="rectangle"
+            />
+          </div>
+          <div className="overlap">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="password"
+              className="rectangle"
+            />
+          </div>
+          <div className="overlap-2" onClick={handleSubmit}>
+            <div className="rectangle-2" />
+            <div className="text-wrapper-3">Login</div>
+          </div>
+        </form>
         <div className="overlap-3">
           <img
             className="img"
@@ -43,6 +54,6 @@ function LoginPage() {
       </div>
     </div>
   );
-};
+}
 
 export default LoginPage;
