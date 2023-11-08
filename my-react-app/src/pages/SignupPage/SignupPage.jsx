26cca09f29a968a60ab9dee03f8f4486530b3ea0
Manav Modi <mfm190002@utdallas.edu>
2023-11-08 17:15:12 -0600

    Updated signup page

--- my-react-app/src/pages/SignupPage/SignupPage.jsx
+++ my-react-app/src/pages/SignupPage/SignupPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./SignupPage.css";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function SignupPage() {
