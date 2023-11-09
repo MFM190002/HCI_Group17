411443a533f2d0a3229b057b5c435d160c892392
Manav Modi <mfm190002@utdallas.edu>
2023-11-09 15:41:59 -0600

    Updated login page

--- my-react-app/src/pages/LoginPage/LoginPage.jsx
+++ my-react-app/src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import "./LoginPage.css";
-import { Link } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate()
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Submitted credentials:", username, password);
 
     // TODO: Handle authentication logic herenp
+  };
+
+  const handleButtonClick = () => {
+    navigate("/signup");
   };
 
   return (
@@ -41,15 +46,9 @@ function LoginPage() {
             <div className="text-wrapper-3">Login</div>
           </div>
         </form>
-        <div className="overlap-3">
-          <img
-            className="img"
-            alt="Rectangle"
-            src="https://cdn.animaapp.com/projects/65408ceb2ddc83e6bd71570e/releases/65408e081cff822e1265c0f3/img/rectangle-8.svg"
-          />
-          <Link to="/signup" className="text-wrapper-4">
-            Sign Up
-          </Link>
+        <div className="overlap-3" onClick={handleButtonClick}>
+          <div className="rectangle-2" />
+          <div className="text-wrapper-3">Sign Up</div>
         </div>
       </div>
     </div>
