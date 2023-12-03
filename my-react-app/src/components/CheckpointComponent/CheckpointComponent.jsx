0619361e5cab3a8b63cc0887743493dbe3445676
Manav Modi <mfm190002@utdallas.edu>
2023-12-03 13:35:54 -0600

    Updated checkpoints page and checkpoint component

--- my-react-app/src/components/CheckpointComponent/CheckpointComponent.jsx
+++ my-react-app/src/components/CheckpointComponent/CheckpointComponent.jsx
@@ -1,17 +1,93 @@
 // CheckpointComponent.js
-import React from 'react';
+import React, { useState } from 'react';
 import './CheckpointComponent.css';
 
-const CheckpointComponent = ({ checkpoint, onCheckClick }) => {
+const CheckpointComponent = ({ checkpoint, onCheckClick, onCheckDelete, completed }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [editedCheckpoint, setEditedCheckpoint] = useState(checkpoint);
+
+  const handleClick = () => {
+    // Only allow clicking if the checkpoint is not completed
+    if (!completed) {
+      onCheckClick(checkpoint);
+    }
+  };
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+    setEditing(false); // Close editing mode when opening/closing dropdown
+  };
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleEditConfirm = () => {
+    // Add your logic for handling the edited name confirmation
+    // For example, you might want to update the checkpoint name in the state or trigger an API call
+    console.log('Edited checkpoint:', editedCheckpoint);
+    setEditing(false); // Exit editing mode
+    setDropdownOpen(false);
+  };
+
+  const handleDelete = () => {
+    onCheckDelete(checkpoint);
+    setDropdownOpen(false);
+  };
+
+  const handleInputChange = (event) => {
+    setEditedCheckpoint(event.target.value);
+  };
+
+  const handleInputKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevent the default form submission behavior
+      handleEditConfirm();
+    }
+  };
+
+  const cursorStyle = completed ? 'default' : 'pointer';
+
   return (
-    <div className="home-checkpoint">
+    <div className={`home-checkpoint ${completed ? 'completed' : ''}`}>
       <div
-        className="home-checkpoint-icon"
-        onClick={() => onCheckClick(checkpoint)}
-        style={{ cursor: 'pointer' }}
-      ></div>
-      <div className="home-checkpoint-text">
-        {checkpoint}
+        className="home-checkpoint-text"
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
+        <div
+          className="home-checkpoint-icon"
+          onClick={handleClick}
+          style={{ cursor: cursorStyle, marginRight: '10px' }}
+        >
+        </div>
+        {editing ? (
+          <input
+            type="text"
+            value={editedCheckpoint}
+            onChange={handleInputChange}
+            onKeyDown={handleInputKeyDown}
+            onBlur={handleEditConfirm}
+            style={{ height: '40px', width: '200px', fontSize: '16px' }} // Adjust the height and width as needed
+            autoFocus
+          />
+        ) : (
+          <>
+            <span>{editedCheckpoint}</span>
+            {completed && <span style={{ marginLeft: '10px' }}>✅</span>}
+          </>
+        )}
+        <div className="dropdown" style={{ marginLeft: '10px'}}>
+          <button className="dropdown-btn" onClick={handleDropdownToggle}>
+            ...
+          </button>
+          {dropdownOpen && (
+            <div className="dropdown-content">
+              <button onClick={handleEdit}>Edit</button>
+              <button onClick={handleDelete}>Delete</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- my-react-app/src/pages/CheckpointsPage/CheckpointsPage.jsx
+++ my-react-app/src/pages/CheckpointsPage/CheckpointsPage.jsx
@@ -1,9 +1,9 @@
+// CheckpointsPage.jsx
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './CheckpointsPage.css';
 import Header from '../../components/Header/Header';
 import CheckpointComponent from '../../components/CheckpointComponent/CheckpointComponent';
-import ProgressComponent from '../HomePage/ProgressComponent/ProgressComponent';
+import './CheckpointsPage.css';
 
 const college_checkpoints = [
   "Create a resume",
@@ -22,82 +22,85 @@ function CheckpointsPage() {
   const [completedCheckpoints, setCompletedCheckpoints] = useState([]);
   const [checkpoints, setCheckpoints] = useState([]);
 
-  const queryParams = new URLSearchParams(window.location.search);
-  const username = queryParams.get('username');
-
   useEffect(() => {
     const initializeLocalStorage = () => {
-      // Check if checkpoints are already in localStorage
       const storedCheckpoints = localStorage.getItem('checkpoints');
       if (!storedCheckpoints) {
-        // If not, initialize localStorage with college_checkpoints
         localStorage.setItem('checkpoints', JSON.stringify(college_checkpoints));
         setCheckpoints(college_checkpoints);
       } else {
         setCheckpoints(JSON.parse(storedCheckpoints));
       }
 
-      // Fetch completed checkpoints from local storage
       const storedCompletedCheckpoints = localStorage.getItem('completedCheckpoints') || '[]';
       setCompletedCheckpoints(JSON.parse(storedCompletedCheckpoints));
     };
 
     initializeLocalStorage();
-  }, [username]);
+  }, []);
 
   const handleCheckpointClick = (clickedCheckpoint) => {
-    // Display confirmation pop-up
-    const isConfirmed = window.confirm(`You are completing this checkpoint: 
-    ${clickedCheckpoint}. 
-    Confirm?`);
+    const isConfirmed = window.confirm(`You are completing this checkpoint: ${clickedCheckpoint}. Confirm?`);
     
     if (!isConfirmed) {
-      return; // Do nothing if the user cancels the confirmation
+      return;
     }
 
     try {
-      // Update local state
       const updatedCompletedCheckpoints = [...completedCheckpoints, clickedCheckpoint];
       setCompletedCheckpoints(updatedCompletedCheckpoints);
 
-      // Calculate progress percentage
-      const progressPercentage = (updatedCompletedCheckpoints.length / checkpoints.length) * 100;
+      const updatedCheckpoints = checkpoints.filter(cp => cp !== clickedCheckpoint);
+      setCheckpoints(updatedCheckpoints);
 
-      // Set progress and completed checkpoints to localStorage
+      const progressPercentage = (updatedCompletedCheckpoints.length / college_checkpoints.length) * 100;
+
       localStorage.setItem('completedCheckpoints', JSON.stringify(updatedCompletedCheckpoints));
       localStorage.setItem('progress', progressPercentage);
+      localStorage.setItem('checkpoints', JSON.stringify(updatedCheckpoints));
     } catch (error) {
       console.error('Error completing checkpoint:', error);
     }
   };
 
-  // Filter out completed checkpoints from the list
-  const remainingCheckpoints = checkpoints.filter(
-    (checkpoint) => !completedCheckpoints.includes(checkpoint)
-  );
+  const handleCheckpointDelete = (deletedCheckpoint) => {
+    const isConfirmed = window.confirm(`Are you sure you want to delete "${deletedCheckpoint}"?`);
+    
+    if (isConfirmed) {
+      try {
+        const updatedCheckpoints = checkpoints.filter(cp => cp !== deletedCheckpoint);
+        setCheckpoints(updatedCheckpoints);
+
+        localStorage.setItem('checkpoints', JSON.stringify(updatedCheckpoints));
+      } catch (error) {
+        console.error('Error deleting checkpoint:', error);
+      }
+    }
+  };
 
   const renderCheckpoints = () => {
-    // Display only the first three checkpoints
-    const displayedCheckpoints = remainingCheckpoints;
-    return displayedCheckpoints.map((checkpoint, index) => (
-      <CheckpointComponent key={index} checkpoint={checkpoint} onCheckClick={handleCheckpointClick} />
+    return checkpoints.map((checkpoint, index) => (
+      <CheckpointComponent
+        key={index}
+        checkpoint={checkpoint}
+        onCheckClick={handleCheckpointClick}
+        onCheckDelete={handleCheckpointDelete}
+        completed={completedCheckpoints.includes(checkpoint)}
+      />
     ));
   };
 
   return (
     <div className="checkpoints-page-1">
-      <Header username={username} />
-      <div className="checkpoints-progress">
-          <ProgressComponent />
-      </div>
+      <Header username={'SampleUser'} />
       <div className="content-container">
         <div className="page-checkpoints-list">
-          <div className="checkpoints-title" >
+          <div className="checkpoints-title">
             Your Upcoming Checkpoints
           </div>
           <div className="page-checkpoint-container">{renderCheckpoints()}</div>
         </div>
-        <Link to={`/add_checkpoint?username=${username}`} className="add-checkpoint-button">
+        <Link to={`/add_checkpoint?username=SampleUser`} className="add-checkpoint-button">
           Add Checkpoints
         </Link>
       </div>
